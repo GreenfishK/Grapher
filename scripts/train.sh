44028880bb9058df7bc6cb57b3913d6ee2820c92
Filip Kovacevic <greenfish427@gmail.com>
2024-02-20 10:42:40 +0100

    update env variables

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# 1 ... edge classifier head; 0 ... edge generation head
+export MODEL_VARIANT=1
+
+# Available hardware settings that are defined below
+# No SLURM; one node; two GPUs per node; Nvidia A40: 1_2_a40
+# No SLURM; one node;  two GPUs per node; Nvidia A100: 1_2_a100
+# No SLURM; one node; two GPUs per node; Nvidia TI: 1_2_ti
+# SLURM; two node; two GPUs per node; Nvidia A40: s2_2_a40
+# SLURM; one node; two GPUs per node; Nvidia A40: s1_2_a40
+# SLURM; one node; one GPU per node; Nvidia A40: s1_1_a40
+export HARDWARE_SETTING=s1_1_a40
+
 # Load environment variables including HARDWARE_SETTING for the run
 cd ../src 
 source .env 
@@ -45,21 +57,21 @@ if [[ $HARDWARE_SETTING == s* ]]; then
     # Run the training with sbatch
     if [[ ${NUM_NODES} -gt 1 ]]; then
         sbatch --job-name=train_grapher_${MODEL_VARIANT}_${HARDWARE_SETTING} \
-        --partition=${PARTITION}\
+        --partition=${PARTITION} \
         --qos=${QOS} \
         --nodes=${NUM_NODES} \
         --gres=gpu:${NUM_GPUS_PER_NODE} \
-        --ntasks-per-node=${NUM_GPUS_PER_NODE}\
+        --ntasks-per-node=${NUM_GPUS_PER_NODE} \
         --time=1-0 \
         --mail-type=BEGIN \
         --mail-user=filip.kovacevic@tuwien.ac.at \
         main.slm "${python_args[@]}"
     else
         sbatch --job-name=train_grapher_${MODEL_VARIANT}_${HARDWARE_SETTING} \
-        --partition=${PARTITION}\
+        --partition=${PARTITION} \
         --qos=${QOS} \
         --gres=gpu:${NUM_GPUS_PER_NODE} \
-        --ntasks-per-node=${NUM_GPUS_PER_NODE}\
+        --ntasks-per-node=${NUM_GPUS_PER_NODE} \
         --time=1-0 \
         --mail-type=BEGIN \
         --mail-user=filip.kovacevic@tuwien.ac.at \
